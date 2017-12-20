96f1960fc51b9e485796cfa3fe1c7bd6c1c8b021
Duncan McGreggor <oubiwann@gmail.com>
2017-12-20 15:42:26 -0600

    Added push capability.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject clojusc/mesomatic-testing "0.1.0-SNAPSHOT"
-  :description "A system and integration testing utility library for Mesomatic."
+  :description "A system and integration testing utility library for Mesomatic"
   :url "https://github.com/clojusc/mesomatic-testing"
   :license {
     :name "Apache License, Version 2.0"
