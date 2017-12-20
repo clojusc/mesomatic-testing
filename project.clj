82b4931aa8a72c2925f620175eac686c2e8be79b
Duncan McGreggor <oubiwann@gmail.com>
2017-12-20 13:40:46 -0600

    Added initial files.

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject clojusc/mesomatic-testing "0.1.0-SNAPSHOT"
+  :description "A system and integration testing utility library for Mesomatic."
+  :url "https://github.com/clojusc/mesomatic-testing"
+  :license {
+    :name "Apache License, Version 2.0"
+    :url "https://www.apache.org/licenses/LICENSE-2.0"}
+  :dependencies [
+    [org.clojure/clojure "1.8.0"]])
---
+++ test/mesomatic/testing/core_test.clj
@@ -0,0 +1,7 @@
+(ns mesomatic.testing.core-test
+  (:require [clojure.test :refer :all]
+            [mesomatic-testing.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
