01257b7c3d5c6cb307c7759d0d6b9edc09ce84c1
Duncan McGreggor <oubiwann@gmail.com>
2017-12-21 11:22:32 -0600

    Fixed up tests.

--- test/mesomatic/testing/core_test.clj
+++ test/mesomatic/testing/core_test.clj
@@ -1,7 +1,6 @@
 (ns mesomatic.testing.core-test
   (:require [clojure.test :refer :all]
-            [mesomatic-testing.core :refer :all]))
+            [mesomatic.testing.core :as mesosmatic-testing]))
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (is (= 1 1)))
